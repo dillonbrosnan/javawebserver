1703c9e16fe3eb5aa17d95b76be3fa64b7a99f72
dillonbrosnan <dillonbrosnan@yahoo.com>
2018-02-05 13:40:50 -0800

    functional request object with string and stream inputs. no error checking implemented yet

--- Code/Request.java
+++ Code/Request.java
@@ -1,7 +1,13 @@
 import java.io.BufferedReader;
 import java.io.StringReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Hashtable;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+// import java.*;
 
 public class Request{
 
@@ -12,93 +18,118 @@ public class Request{
   private String httpRequest;
   private String[] parsedTest;
   private Hashtable<String, String> headers;
-  private StringBuffer messageBody;
+  private byte[] messageBody;
   private static final int HEADER_KEY = 0;
   private static final int HEADER_VALUE= 1;
 
   public Request(){
 
   }
-  public Request(String httpRequest){
+
+  public Request( String httpRequest ){
     this.httpRequest = httpRequest;
-  	try{
-  	  parse();
-  	}
-  	catch( IOException ex ){
-  		//TODO implement 400 response handling
-  		System.out.println( ex.toString() );
-  	}
+    try{
+      parse();
+    }
+    catch( IOException ex ){
+      //TODO implement 400 response handling
+      System.out.println( ex.toString() );
+    }
+  }
+
+  public Request( InputStream httpRequestStream ) {
+    //this.httpRequest = IOUtils.toString( httpRequestStream, StandardCharsets.UTF_8 );
+    
+    try{
+      inputStreamToString( httpRequestStream );
+      parse();
+    }
+    catch( IOException ex ){
+      //TODO implement 400 response handling
+      System.out.println( ex.toString() );
+    }
+  }
+  
+  private void inputStreamToString( InputStream httpRequestStream ) throws IOException {
+    try (Scanner scanner = new Scanner( httpRequestStream , StandardCharsets.UTF_8.name() ) ) {
+      this.httpRequest = scanner.useDelimiter( "\\A" ).next();
+    } 
+          
   }
 
   public void parse() throws IOException{
-  	BufferedReader reader = new BufferedReader( new StringReader( httpRequest ) );
-  	String line;
+    BufferedReader reader = new BufferedReader( new StringReader( httpRequest ) );
+    String line;
 
-  	parseRequestLine( reader.readLine() );
+    parseRequestLine( reader.readLine() );
 
-  	headers = new Hashtable<String, String>();
+    headers = new Hashtable<String, String>();
     line = reader.readLine();
-    while( line != "\r\n" && line != null ){
+    while( line.length() > 0 ){
+      System.out.println(line);
       addToHeaders( line );
       line = reader.readLine();
     }
 
-    //TODO incorrect logic according to jrob 
-    //"protocol specifies additional headers to determine how
-    //to read the body which may not be string"
     line = reader.readLine();
+    messageBody = new byte[0];
     while( line != "\r\n" && line != null ){
-    	appendBody( line );
-    	line = reader.readLine();
+      storeBody( line );
+      line = reader.readLine();
     }
   }
 
   private void parseRequestLine(String requestLineParse){
-  	String[] requestLineSubstrings = requestLineParse.split( "\\s" );
+    String[] requestLineSubstrings = requestLineParse.split( "\\s" );
 
-  	setVerb( requestLineSubstrings[0] );
-  	setUri( requestLineSubstrings[1] );
-  	setHttpVersion( requestLineSubstrings[2] );
+    setVerb( requestLineSubstrings[0] );
+    setUri( requestLineSubstrings[1] );
+    setHttpVersion( requestLineSubstrings[2] );
   }
 
   private void addToHeaders( String headerLine ){
-  	String[] headerParts = headerLine.split(": ");  	
-  	this.headers.put( headerParts[HEADER_KEY], headerParts[HEADER_VALUE] );
+    String[] headerParts = headerLine.split(": ");  
+    this.headers.put( headerParts[HEADER_KEY], headerParts[HEADER_VALUE] );
   }
 
-  //TODO "not correct treatment of body, leave as bytestream?"
-  private void appendBody( String bodyLine ){
-    this.messageBody.append( bodyLine ).append( "\r\n" );
+  private void storeBody( String bodyLine ){
+    byte[] bodyLineBytes = bodyLine.getBytes();
+    int totalLength = messageBody.length + bodyLineBytes.length;
+    byte[] destination = new byte[totalLength];
+    
+    System.arraycopy( messageBody, 0, destination, 0, messageBody.length );
+    System.arraycopy( bodyLineBytes, 0, destination, messageBody.length, bodyLineBytes.length );
+    messageBody = Arrays.copyOf( destination, totalLength);
   }
-
   private void setVerb( String verb ){
-  	this.verb = verb;
+    this.verb = verb;
   }
   private void setUri( String uri ){
-  	this.uri = uri;
+    this.uri = uri;
   }
   private void setHttpVersion( String httpVersion ){
-  	this.httpVersion = httpVersion;
+    this.httpVersion = httpVersion;
   }
 
   public String getVerb(){
-  	return this.verb;
+    return this.verb;
   }
   public String getUri(){
-  	return this.uri;
+    return this.uri;
   }
   public String getHttpVersion(){
-  	return this.httpVersion;
+    return this.httpVersion;
   }
 
-
   public void print(){
-  	System.out.println( "Verb: " + this.verb + ", uri: " + this.uri + 
-  	  ", httpVersion: " + this.httpVersion );
+    System.out.println( "Verb: " + this.verb + ", uri: " + this.uri + 
+      ", httpVersion: " + this.httpVersion );
 
-  	for( String key: headers.keySet() ){
-  	  String value = headers.get(key).toString();
-  	  System.out.println( "Key: " + key + ", Value: " + value);
-  	}
+    for( String key: headers.keySet() ){
+      String value = headers.get(key).toString();
+      System.out.println( "Key: " + key + ", Value: " + value);
+    }
+    String messageBodyString = new String( messageBody, Charset.forName("UTF-8") );
+    System.out.println( messageBodyString );
   }
 }
--- Code/Server.java
+++ Code/Server.java
@@ -1,7 +1,29 @@
+import java.io.*;
+
 public class Server{
   public static void main( String[] args ){
-  	Request request = new Request("GET / http/1.1\r\nHost: net.tutsplus.com\r\n" +
-  		"Host2: net.tutsplus.com2\r\n");
-  	request.print();
+    // Request request = new Request("GET / http/1.1\r\nHost: net.tutsplus.com \r\n" +
+    //   "Host2: net.tutsplus.com2\r\n\r\nhome=Cosby&favorite+flavor=flies\r\nhome=Cosby&favorite+flavor=flies\r\n");
+    String s = "GET / http/1.1\r\nHost: net.tutsplus.com \r\n" +
+      "Host2: net.tutsplus.com2\r\n\r\nhome=Cosby&favorite+flavor=flies\r\nhome=Cosby&favorite+flavor=flies\r\n";
+      InputStream is = new ByteArrayInputStream( s.getBytes() );
+      Request request = new Request( is );
+    request.print();
   }
+
+
+//   public void start(){
+//     ServerSocket socket = new ServerSocket( DEFAULT_PORT );
+//     Socket client = null;
+//     while( true ){
+//       client = socket.accept();
+//       outputRequest( client );
+//       // TODO: Response
+//       client.close();
+//     }
+//   }
+
+  // protected static void outputRequest( Socket client ) throws IOException {
+  //   Request request = new Request( new InputStream( client.getInputStream() ) );
+  // }
 }
