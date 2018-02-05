e379efdf7f64de2f33bd44bc81140f9f647a0bed
Dylan Abrames <abrames.dylan@gmail.com>
2018-02-05 13:35:37 -0800

    updated directories

--- Server.java
+++
@@ -1,7 +0,0 @@
-public class Server{
-  public static void main( String[] args ){
-  	Request request = new Request("GET / http/1.1\r\nHost: net.tutsplus.com\r\n" +
-  		"Host2: net.tutsplus.com2\r\n");
-  	request.print();
-  }
-}
